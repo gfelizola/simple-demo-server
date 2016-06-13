bd399743ca86a2e27d795439b5b441a3fc3a114c
Gustavo Felizola <gfelizola@gmail.com>
2016-06-13 20:45:49 -0300

    initial files

---
+++ server.js
@@ -0,0 +1,23 @@
+// server.js
+var jsonServer = require('json-server');
+var server = jsonServer.create();
+var router = jsonServer.router('db.json');
+var middlewares = jsonServer.defaults();
+
+server.use(middlewares)
+
+server.post('/auth', function (req, res) {
+    console.log( req.params );
+    // res.json({ error: "login inválido"});
+    res.json( req.params )
+});
+
+server.get('/echo', function (req, res) {
+  res.jsonp(req.query)
+})
+
+
+server.use(router)
+server.listen(3000, function () {
+  console.log('JSON Server is running')
+})
