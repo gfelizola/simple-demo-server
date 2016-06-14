093192a14b4f214b74a7f5d17dfa87fe15c02815
Gustavo Felizola <gfelizola@gmail.com>
2016-06-13 21:23:28 -0300

    auth validations

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # simple-demo-server
 Simple serve to use for tests and demos
+
+Created with json-server[1]
+
+
+### Starting
+
+cd /simple-demo-server
+npm install
+npm start
+
--- server.js
+++ server.js
@@ -1,23 +1,72 @@
 // server.js
-var jsonServer = require('json-server');
-var server = jsonServer.create();
-var router = jsonServer.router('db.json');
+var jsonServer  = require('json-server');
+var server      = jsonServer.create();
+var router      = jsonServer.router('db.json');
 var middlewares = jsonServer.defaults();
+var bodyParser  = require('body-parser');
+var session     = require('express-session');
+var hash        = require('./hash');
 
+// JSON-Server defaults middlewares (logger, static, cors and no-cache)
 server.use(middlewares)
 
-server.post('/auth', function (req, res) {
-    console.log( req.params );
-    // res.json({ error: "login inválido"});
-    res.json( req.params );
+// For parsing application/json
+server.use(bodyParser.json());
+
+// To save user in session
+server.use(session({secret: 'hue-hue-br'}));
+
+// Do login, and save token and user in session
+server.post('/login', function (req, res) {
+    var b    = req.body,
+        user = b.username,
+        pass = b.password;
+
+    if ( user == 'teste' && pass == '123123' ) {
+        var sess  = req.session,
+            token = hash(32);
+
+        sess.user  = { username: "teste" };
+        sess.token = token;
+
+        res.set('X-AUTH-TOKEN', token);
+        res.json({ message: "Login successful" });
+    } else {
+        res.status(401).json({ error: "Invalid credentials" });
+    }
+
 });
 
-server.get('/echo', function (req, res) {
-  res.jsonp(req.query)
+// To logout
+server.get('/logout', function (req, res) {
+    req.session.destroy(function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.json({message: "Logout successful"});
+        }
+    });
+});
+
+// Validate if is loggedin
+server.use(function (req, res, next) {
+    var url = req.url.toLowerCase();
+
+    if ( url.indexOf('login') < 0 || url.indexOf('logout') < 0 ) {
+        var token = req.session.token;
+        var reqToken = req.headers['x-auth-token'];
+
+        if ( reqToken != token ) {
+            res.status(401).end("Invalid credentials");
+        }
+    }
+
+    // Continue to JSON Server router
+    next()
 })
 
 
 server.use(router)
 server.listen(3000, function () {
-  console.log('JSON Server is running')
+  console.log('JSON Server is running. Goto http://localhost:3000');
 })
